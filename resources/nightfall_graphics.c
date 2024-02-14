6d6c8a3bf892b0da1f68fe5c669550c7622a544c
bwitruk <bwitruk@gmail.com>
2024-02-13 19:58:15 -0500

    swap to 320AC

--- resources/nightfall_graphics.c
+++ resources/nightfall_graphics.c
@@ -1,10 +1,3 @@
-#include "stdlib.h"
-#include "string.h"
-#include "prosystem.h"
-#include "joystick.h"
-
-#define MODE_320AC
-
 const char nightfall_tiles_0_0[1] = {0};
 const char nightfall_tiles_0_1[1] = {2};
 const char nightfall_tiles_0_2[1] = {4};
@@ -22,9 +15,11 @@ const char nightfall_tiles_data_ptrs_low[14] = {nightfall_tiles_0_data & 0xff, n
 
 const char *nightfall_tiles_data_ptrs[2] = {nightfall_tiles_data_ptrs_high, nightfall_tiles_data_ptrs_low};
 
+/*
 #define TILING_HEIGHT 14
 #define TILING_WIDTH 20
 #include "sparse_tiling.h"
+*/
 
 reversed scattered(16,2) char nightfall_001[32] = {
 	0xaa, 0xae, 0x11, 0x55, 0xaa, 0xbb, 0x04, 0x55, 0xaa, 0xaa, 0x01, 0x55, 0xaa, 0xab, 0x00, 0x55,
@@ -54,66 +49,3 @@ reversed scattered(16,4) char nightfall_006[64] = {
 	0xc4, 0x32, 0xc8, 0x33, 0x00, 0xfa, 0x88, 0xff, 0x12, 0xea, 0x13, 0xef, 0x7a, 0x88, 0x7f, 0x8c,
 	0xfa, 0x00, 0xff, 0x00, 0xc8, 0x33, 0xcc, 0x00, 0x00, 0x7f, 0x8c, 0x00, 0x00, 0x6f, 0x00, 0x00
 };
-
-#define BACKGROUND_FLIP 50
-
-ramchip int background_ticker;
-ramchip int x, y;
-ramchip char background;
-ramchip char background_increment;
-
-void background_step() {
-	if (background_ticker == BACKGROUND_FLIP) {
-		background = background + background_increment;
-
-		if (background == 0x0)
-			background_increment = 1;
-		else if (background == 0x0f)
-			background_increment = -1;
-
-		*BACKGRND = background;
-
-		background_ticker = -1;
-	}
-	background_ticker++;
-}
-
-void main()
-{
-    x = 0; y = 0;
-	background_ticker = 0;
-	background = 0x0f;
-	background_increment = -1;
-
-    multisprite_init();
-    multisprite_set_charbase(nightfall_001);
-    sparse_tiling_init(nightfall_tiles_data_ptrs);
-    joystick_init();
-    
-    sparse_tiling_goto(x, y);
- 
-	//*BACKGRND = 0x0;
-	*BACKGRND = background;
-
-    *P0C2 = multisprite_color(0xbc); // Light turquoie
-    *P1C2 = multisprite_color(0x33); // Red
-    *P2C2 = multisprite_color(0xa5); // Dark turquoise
-    *P3C2 = multisprite_color(0x83); // Blue
-    *P4C2 = 0x0f; // White
-    *P5C2 = 0x00; // Black
-    *P6C2 = multisprite_color(0x29); // Orange 
-    *P7C2 = multisprite_color(0x3c); // Rose 
-
-    // Main loop
-    do {
-		background_step();
-
-        multisprite_flip();
-        joystick_update();
-        if (joystick[0] & JOYSTICK_LEFT) x--; 
-        else if (joystick[0] & JOYSTICK_RIGHT) x++;
-        if (joystick[0] & JOYSTICK_UP) y--; 
-        else if (joystick[0] & JOYSTICK_DOWN) y++;
-        sparse_tiling_goto(x, y);
-    } while(1);
-}
